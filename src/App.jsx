f6686b88ee435346d907f5e00138f19e65e97ebe
Ayeza Bashir <ayezabashir46@gmail.com>
2026-02-14 19:07:37 +0500

    configured routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-// import HomePage from "./pages/HomePage";
-// import DetailsPage from "./pages/DetailsPage";
-
+import HomePage from "./pages/HomePage";
+import DetailsPage from "./pages/DetailsPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RoadmapPage from "./pages/RoadmapPage";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-50 p-4 md:p-8">
-      <main>
-        {/* <HomePage /> */}
-        {/* <DetailsPage /> */}
-        <RoadmapPage />
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50 p-4 md:p-8">
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="details" element={<DetailsPage />} />
+            <Route path="roadmap" element={<RoadmapPage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
