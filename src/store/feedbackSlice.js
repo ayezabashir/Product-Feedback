91d4524875d6287edc3e7cb24662c9c820d14bfc
Ayeza Bashir <ayezabashir46@gmail.com>
2026-02-14 20:48:25 +0500

    suggestions reducer functions

--- src/store/feedbackSlice.js
+++ src/store/feedbackSlice.js
@@ -9,7 +9,49 @@ const initialState = {
 const feedbackSlice = createSlice({
   name: "feedback",
   initialState,
-  reducers: {},
+  reducers: {
+    addSuggestion: (state, action) => {
+      const newSuggestion = {
+        id: Date.now(),
+        title: action.payload.title,
+        description: action.payload.description,
+        upvotes: 0,
+        comments: 0,
+        category: action.payload.category || "Feature",
+        upvoted: false,
+        status: action.action.status || "Planned",
+      };
+      state.suggestions.push(newSuggestion);
+    },
+    updateSuggestion: (state, action) => {
+      const index = state.suggestions.findIndex(
+        (suggestion) => suggestion.id === action.payload.id,
+      );
+      if (index !== -1) {
+        state.suggestions[index] = {
+          ...state.suggestions[index],
+          ...action.payload,
+        };
+      }
+    },
+    deleteSuggestion: (state, action) => {
+      state.suggestions = state.suggestions.filter(
+        (suggestion) => suggestion.id !== action.payload,
+      );
+      delete state.suggestions[action.payload];
+    },
+    toggleUpvote: (state, action) => {
+      const item = state.suggestions.find(
+        (suggestion) => suggestion.id === action.payload,
+      );
+      if (item) {
+        item.upvoted = !item.upvoted;
+        item.upvotes = item.upvoted
+          ? item.upvotes + 1
+          : Math.max(0, item.upvotes - 1);
+      }
+    },
+  },
 });
 
-export default feedbackSlice.reducer
+export default feedbackSlice.reducer;
